bfbb445059ca7c74e66aa072bdc6b0db084638ea
dmkomur <125804681+dmkomur@users.noreply.github.com>
2023-05-17 18:18:35 +0300

    fix proptype

--- src/components/FriendsList/FriendsList.jsx
+++ src/components/FriendsList/FriendsList.jsx
@@ -1,9 +1,15 @@
 import { Friends } from './FriendsList.styled'
 import { FriendsListItem } from '../FriendsListItem/FriendListItem'
+import PropTypes from 'prop-types';
 
 
 export const FriendsList = ({friends}) => {
   return <Friends>   
     {friends.map(el => (<FriendsListItem {...el} />))}
 </Friends>
+}
+
+
+FriendsList.propTypes = {
+  friends: PropTypes.array
 }
